940a6641b2771ab2ac73e9bace5f23e881796d0e
Yun <yun.cho62@gmail.com>
2022-04-25 21:48:19 -0400

    Initial version of NYCEtube generated by generator-jhipster@7.8.1

---
+++ NYCEtube/src/main/java/com/nyce/tube/service/EmailAlreadyUsedException.java
@@ -0,0 +1,10 @@
+package com.nyce.tube.service;
+
+public class EmailAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public EmailAlreadyUsedException() {
+        super("Email is already in use!");
+    }
+}
---
+++ NYCEtube/src/main/java/com/nyce/tube/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.nyce.tube.service.dto;
