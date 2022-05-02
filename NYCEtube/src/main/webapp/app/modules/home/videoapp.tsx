7d712be4275c10ba17c83012e3166e860c4df456
nusera <nusera.neha@gmail.com>
2022-05-01 22:14:01 -0400

    files added?

---
+++ NYCEtube/src/main/webapp/app/modules/home/videoapp.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import VideoPlayer from 'react-video-js-player';
+
+class VideoApp extends Component {
+    player = {}
+    state = {
+        video: {
+            src: 'src/main/webapp/content/videosample/sora',
+            poster: '/webapp/content/images/jhipster_family_member_0_head-192.png'
+        }
+    }
+
+    onPlayerReady(player){
+        this.player = player;
+    }
+
+    render() {
+        return (
+            <div>
+                <VideoPlayer
+                    controls={true}
+                    src={this.state.video.src}
+                    poster={this.state.video.poster}
+                    width="720"
+                    height="420"
+                    onReady={this.onPlayerReady.bind(this)}
+                    type="video/mp4"
+                />
+            </div>
+        );
+    }
+}
+export default VideoApp;
